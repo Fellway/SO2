bcfecfba55396c841655db4907fca5408aeec756
Fellway <mateusz.skrzypczyk1@hotmail.com>
2020-09-03 20:17:17 +0200

    feature/SO2-LAB5 Min and Max added

--- app/Calculator.h
+++ app/Calculator.h
@@ -6,6 +6,8 @@ public:
     Calculator();
 
     static void avg(const int numbers[], const int arraySize);
+    static void max(const int numbers[], const int arraySize);
+    static void min(const int numbers[], const int arraySize);
 };
 
 #endif
--- app/main.cpp
+++ app/main.cpp
@@ -49,6 +49,8 @@ void monitor(int N) {
                 readFile(hFile, readBuffer);
                 convertToNumbers(readBuffer, numbers);
                 Calculator::avg(numbers, N);
+                Calculator::max(numbers, N);
+                Calculator::min(numbers, N);
                 CloseHandle(hFile);
             }
         } else {
@@ -60,6 +62,30 @@ void monitor(int N) {
     exit(0);
 }
 
+int writer(char *argv[]) {
+    PROCESS_INFORMATION information;
+    HANDLE hFile;
+    DWORD dwNumberOfByteWritten = 0;
+    hFile = CreateFile(FILE_NAME, FILE_WRITE_DATA, 0, nullptr, OPEN_ALWAYS,
+                       FILE_ATTRIBUTE_NORMAL,
+                       nullptr);
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("CreateFile failed (%lu).\n", GetLastError());
+        return -1;
+    }
+    information = Process::startProcess(argv[0], argv[1], READER_PROCESS_NAME);
+    RandomPicker randomPicker = RandomPicker();
+    for (int i = 0; i < atoi(argv[1]); i++) {
+        string randomNumber = std::to_string(randomPicker.getRandomNumber(1, 100)) + CORRECTOR;
+        WriteFile(hFile, randomNumber.c_str(), strlen(randomNumber.c_str()),
+                  &dwNumberOfByteWritten,
+                  nullptr);
+    }
+    CloseHandle(hFile);
+    WaitForSingleObject(information.hProcess, INFINITE);
+}
+
 int main(int argc, char *argv[]) {
     if (argc == 3 && strcmp(argv[2], READER_PROCESS_NAME) == 0) {
         printf("Hello from %s.\n", argv[2]);
@@ -67,26 +93,6 @@ int main(int argc, char *argv[]) {
             monitor(atoi(argv[1]));
         }
     } else {
-        PROCESS_INFORMATION information;
-        HANDLE hFile;
-        DWORD dwNumberOfByteWritten = 0;
-        hFile = CreateFile(FILE_NAME, FILE_WRITE_DATA, 0, nullptr, OPEN_ALWAYS,
-                           FILE_ATTRIBUTE_NORMAL,
-                           nullptr);
-
-        if (hFile == INVALID_HANDLE_VALUE) {
-            printf("CreateFile failed (%lu).\n", GetLastError());
-            return -1;
-        }
-        information = Process::startProcess(argv[0], argv[1], READER_PROCESS_NAME);
-        RandomPicker randomPicker = RandomPicker();
-        for (int i = 0; i < atoi(argv[1]); i++) {
-            string randomNumber = std::to_string(randomPicker.getRandomNumber(1, 100)) + CORRECTOR;
-            WriteFile(hFile, randomNumber.c_str(), strlen(randomNumber.c_str()),
-                      &dwNumberOfByteWritten,
-                      nullptr);
-        }
-        CloseHandle(hFile);
-        WaitForSingleObject(information.hProcess, INFINITE);
+       writer(argv);
     }
 }
